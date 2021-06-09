04dcc9d753eca14ab29f6f58ddcb6e0fb2241245
mbelitz <mbelitz@ufl.edu>
2021-06-09 12:38:45 -0400

    add failedaccessiblearea function that plots covex hull if alpha hull fails

--- scripts/dataMunging/clean_NA_OccurrenceRecords.R
+++ scripts/dataMunging/clean_NA_OccurrenceRecords.R
@@ -2,6 +2,7 @@ library(rnaturalearth)
 
 source("scripts/pipelineFunctions/02_clean_occurrences.R")
 source("scripts/pipelineFunctions/03_define_accessibleArea.R")
+source('scripts/pipelineFunctions/03-5_define_accessibleArea_alphaHullFail.R')
 
 nco <- read_csv("Occ_Data/NA_OC_All_Records_2020-10-16.csv")
 
@@ -67,6 +68,61 @@ makemaps <- function(binomial){
   
 }
 
+
+makemaps_failedAccessibleArea <- function(binomial){
+  
+  spdf <- nco %>% 
+    filter(scientific_name == binomial)
+  
+  cc <- coord_clean(species = binomial, df = nco)
+  
+  p1 <- ggplot() +
+    geom_sf(world, mapping = aes()) +
+    geom_point(spdf, mapping = aes(x = decimalLongitude, y = decimalLatitude),
+               color = "purple")  +
+    coord_sf(xlim = c(min(spdf$decimalLongitude) - 10, max(spdf$decimalLongitude) + 10),
+             ylim = c(min(spdf$decimalLatitude) - 10, max(spdf$decimalLatitude) + 10)) +
+    ggtitle("Uncleaned") +
+    theme_bw()
+  
+  p2 <- ggplot() +
+    geom_sf(world, mapping = aes()) +
+    geom_point(cc, mapping = aes(x = decimalLongitude, y = decimalLatitude),
+               color = "purple")  +
+    coord_sf(xlim = c(min(cc$decimalLongitude) - 10, max(cc$decimalLongitude) + 10),
+             ylim = c(min(cc$decimalLatitude) - 10, max(cc$decimalLatitude) + 10)) +
+    ggtitle("Cleaned") +
+    theme_bw()
+  
+  aa_shp <- define_FailedaccessibleArea(species_df = cc, minBuff = 100000,
+                                  saveImage = FALSE,
+                                  saveShapefile = FALSE)
+  
+  p3 <- ggplot() + 
+    geom_sf(world, mapping = aes(), fill = NA) +
+    geom_sf(st_as_sf(aa_shp), mapping = aes(), fill = "Orange") +
+    geom_point(cc, mapping = aes(x = decimalLongitude, y = decimalLatitude),
+               color = "black")  +
+    coord_sf(xlim = c(min(cc$decimalLongitude) - 10, max(cc$decimalLongitude) + 10),
+             ylim = c(min(cc$decimalLatitude) - 10, max(cc$decimalLatitude) + 10)) +
+    ggtitle(binomial) +
+    theme_classic()
+  
+  cp <- cowplot::plot_grid(p1, p2, p3, rel_heights = c(1,1,3), rel_widths = c(1,1,3))
+  
+  dir.create(paste("Outputs/CleanedOccs/AllNorthAmericaSpp/", binomial, sep = ""))
+  
+  ggsave(filename = paste("Outputs/CleanedOccs/AllNorthAmericaSpp/", 
+                          binomial, "/CoordClean.png", 
+                          sep = ""),
+         plot = cp)
+  
+  write.csv(x = cc, file = paste("Outputs/CleanedOccs/AllNorthAmericaSpp/", 
+                                 binomial, "/occurrenceRecords.csv", 
+                                 sep = ""),
+            row.names = F)
+  
+}
 
 ## read in species list
 library(googlesheets4)
